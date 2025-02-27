7e2fde2ff2bb4ee01340f3d2b8a3f1f1e072bc8f
Amparo Oliver <amparooliverb@gmail.com>
2025-02-27 11:40:43 -0300

    404

--- my-cv-project/vite.config.js
+++ my-cv-project/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CV/', // This should match your repository name
+  base: '/CV/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
